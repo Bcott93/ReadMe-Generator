717a959d709e17777817f70c4855f8543ef66f13
Benjamin <bcott93@gmail.com>
2023-02-24 14:37:56 +0000

    initalised all packages, checked FS function and added relevant questions, started creating the boiler plate for the read me

--- starter/index.js
+++ starter/index.js
@@ -1,21 +1,75 @@
-const fs = require("fs");
-const path = require('path');
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+// Access Fileshare module
+import fs from 'fs'
+// Access Path module
+// const path = require('path');
+// import path from 'path'
+// Access Inquirer Module
+// const inquirer = require("inquirer");
+import inquirer from 'inquirer'
+// Access the generateMarkdown JS file
+// import generateMarkdown from "./utils/generateMarkdown"
 
 // array of questions for user
-const questions = [
+inquirer 
+    .prompt ([   
+        {
+            type: 'input',
+            message: 'What is your project title?',
+            name: 'title',
+        },
+        // {
+        //     type: 'input',
+        //     message: 'Please input your description',
+        //     name: 'description',
+        // },
+        // {
+        //     type: 'input',
+        //     message: 'Section: Installation',
+        //     name: 'installation',
+        // },
+        // {
+        //     type: 'input',
+        //     message: 'Section: Usage',
+        //     name: 'Usage',
+        // },
+        // {
+        //     type: 'input',
+        //     message: 'Section: License',
+        //     name: 'license',
+        // },
+        // {
+        //     type: 'input',
+        //     message: 'Section: Contributions',
+        //     name: 'contributions',
+        // },
+        // {
+        //     type: 'input',
+        //     message: 'Section: Tests',
+        //     name: 'tests',
+        // },
+        // {
+        //     type: 'input',
+        //     message: 'Section: Questions',
+        //     name: 'questions',
+        // },
 
-];
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+])
 
-// function to initialize program
-function init() {
+.then((response) => {
+    fs.writeFile("test12.md", 
+    JSON.stringify(response.title), 
+    (err) => err ? console.error(err) : console.log('Success'))
+    
+    
+    
+})
 
-}
 
-// function call to initialize program
-init();
+// // function to initialize program
+// function init() {
+
+// }
+
+// // function call to initialize program
+// init();
