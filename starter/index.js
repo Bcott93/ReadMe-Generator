5a4ba8cc1e29e63bc0ddf0998d824323adf91d5f
Benjamin <bcott93@gmail.com>
2023-02-24 14:51:51 +0000

    finished ReadMe template, imported to JS, template literals work

--- starter/index.js
+++ starter/index.js
@@ -17,48 +17,79 @@ inquirer
             message: 'What is your project title?',
             name: 'title',
         },
-        // {
-        //     type: 'input',
-        //     message: 'Please input your description',
-        //     name: 'description',
-        // },
-        // {
-        //     type: 'input',
-        //     message: 'Section: Installation',
-        //     name: 'installation',
-        // },
-        // {
-        //     type: 'input',
-        //     message: 'Section: Usage',
-        //     name: 'Usage',
-        // },
-        // {
-        //     type: 'input',
-        //     message: 'Section: License',
-        //     name: 'license',
-        // },
-        // {
-        //     type: 'input',
-        //     message: 'Section: Contributions',
-        //     name: 'contributions',
-        // },
-        // {
-        //     type: 'input',
-        //     message: 'Section: Tests',
-        //     name: 'tests',
-        // },
-        // {
-        //     type: 'input',
-        //     message: 'Section: Questions',
-        //     name: 'questions',
-        // },
+        {
+            type: 'input',
+            message: 'Please input your description:',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'Section - Installation:',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'Section - Usage:',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'Section - License:',
+            name: 'license',
+        },
+        {
+            type: 'input',
+            message: 'Section - Contributions:',
+            name: 'contributions',
+        },
+        {
+            type: 'input',
+            message: 'Section - Tests:',
+            name: 'tests',
+        },
+        {
+            type: 'input',
+            message: 'Section - Questions:',
+            name: 'questions',
+        },
 
 
 ])
 
 .then((response) => {
     fs.writeFile("test12.md", 
-    JSON.stringify(response.title), 
+    `# ${response.title}
+
+## Description
+
+     ${response.description}
+
+
+## Table of contents
+
+    1. [Installation](#installation)
+    2. [Usage](#usage)
+    3. [License](#license)
+    4. [Contributions](#contributions)
+    5. [Tests](#tests)
+    6. [Questions](#questions)
+
+## Installation <a id="installation"></a>
+     ${response.installation}
+
+## Usage <a id="usage"></a>
+     ${response.usage}
+
+## License <a id="license"></a>
+     ${response.license}
+
+## Contributions <a id="contributions"></a>
+     ${response.contributions}
+
+## Tests <a id="tests"></a>
+     ${response.tests}
+## Questions <a id="questions"></a>
+     ${response.questions}`, 
     (err) => err ? console.error(err) : console.log('Success'))
     
     
--- starter/test12.md
+++ starter/test12.md
@@ -1,1 +1,32 @@
-"1"
+# Readme-Generator
+
+## Description
+
+     The idea is to create a readme
+
+
+## Table of contents
+
+    1. [Installation](#installation)
+    2. [Usage](#usage)
+    3. [License](#license)
+    4. [Contributions](#contributions)
+    5. [Tests](#tests)
+    6. [Questions](#questions)
+
+## Installation <a id="installation"></a>
+     Node.js
+
+## Usage <a id="usage"></a>
+     undefined
+
+## License <a id="license"></a>
+     mit
+
+## Contributions <a id="contributions"></a>
+     me
+
+## Tests <a id="tests"></a>
+     me
+## Questions <a id="questions"></a>
+     nope
