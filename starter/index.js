ae281b7cf1997d53bd4781f50b9fe17453913aa3
Benjamin <bcott93@gmail.com>
2023-03-01 01:48:26 +0000

    moved the markdown to a seperate JS folder, linked it to the index, fixed the badge issue by reading and slicing the created file

--- starter/index.js
+++ starter/index.js
@@ -1,114 +1,153 @@
-// Access Fileshare module
+// Access FS module
 import fs from 'fs'
 // Access Path module
-// const path = require('path');
-// import path from 'path'
+import path from 'path'
 // Access Inquirer Module
-// const inquirer = require("inquirer");
 import inquirer from 'inquirer'
 // Access the generateMarkdown JS file
-// import generateMarkdown from "./utils/generateMarkdown"
+import generateMarkdown from "./utils/generateMarkdown.js"
 
-// array of questions for user
+// Ternary operator to check if the the userInput has a value or not
+function validateUserInput(input) {
+    return (input !== "") ? true : ("I did not detect a value, please enter a valid input")
+}
+// Ternary operator to check if the the userInput includes a @ or not
+function validateEmail(input) {
+    return (input.includes("@")) ? true : ("I did not detect a valid email address, please try again")
+}
+
+const licenses = ["MIT", "CCO-1.0", "EPL 1.0"]
+
+const licenseInfo = [
+    {
+        licName: licenses[0],
+        icon: "https://img.shields.io/badge/License-MIT-yellow.svg",
+        link: "https://opensource.org/licenses/MIT",
+    },
+    {
+        licName: licenses[1],
+        icon: "https://licensebuttons.net/l/zero/1.0/80x15.png",
+        link: "http://creativecommons.org/publicdomain/zero/1.0/",
+    },
+    {
+        licName: licenses[2],
+        icon: "https://img.shields.io/badge/License-EPL_1.0-red.svg",
+        link: "https://opensource.org/licenses/EPL-1.0",
+    },
+]
+
+let licIcon = ""
+let licLink = ""
+
+function setIcon(response) {   
+    if (response.license === licenses[0]) {
+    licIcon = licenseInfo[0].icon
+    licLink = licenseInfo[0].link
+   
+    } else {
+        if (response.license === licenses[1]) {
+             licIcon = licenseInfo[1].icon
+             licLink = licenseInfo[1].link
+       
+} else {
+    licIcon = licenseInfo[2].icon
+    licLink = licenseInfo[2].link
+}
+}
+return
+}
+
+
+
 inquirer 
     .prompt ([   
         {
             type: 'input',
+            message: 'What is your name?',
+            name: 'name',
+            validate: validateUserInput,
+        },
+        {
+            type: 'input',
             message: 'What is your project title?',
             name: 'title',
+            validate: validateUserInput,
         },
         {
             type: 'input',
             message: 'Please input your description:',
             name: 'description',
+            validate: validateUserInput,
         },
         {
             type: 'input',
-            message: 'Section - Installation:',
+            message: 'Please detail installation instructions:',
             name: 'installation',
+            validate: validateUserInput,
         },
         {
             type: 'input',
-            message: 'Section - Usage:',
+            message: 'Please detail usage information:',
             name: 'usage',
+            validate: validateUserInput,
         },
        
         {
             type: 'input',
-            message: 'Section - Contributions:',
+            message: 'Please detail contribution guidelines:',
             name: 'contributions',
+            validate: validateUserInput,
         },
         {
             type: 'input',
-            message: 'Section - Tests:',
+            message: 'Please detail test instructions:',
             name: 'tests',
+            validate: validateUserInput,
         },
         {
             type: 'input',
             message: 'Please enter your github username:',
             name: 'github',
+            validate: validateUserInput,
         },
         {
             type: 'input',
             message: 'Please enter your email address:',
             name: 'email',
+            validate: validateEmail,
         },
-        // {
-        //     type: 'input',
-        //     message: 'Section - License:',
-        //     name: 'license',
-        // },
-
-
+        {
+            type: 'list',
+            message: 'Please choose a license from the list;',
+            name: 'license',
+            choices: licenses,     
+     },
 ])
 
 .then((response) => {
-    fs.writeFile("test12.md", 
-    `# ${response.title}
+    const markdown = generateMarkdown(response)
+    fs.writeFile(`${response.name}.md`,
+    markdown,
+    (err) => err ? console.error(err) : console.log('Success'))   
 
-## Description
+    fs.readFile(`${response.name}.md`,
+    'utf-8',
+    (err, data) => {
+        if (err) {
+            console.error(err)
+        } else {
 
-     ${response.description}
-
-
-## Table of contents
-
-1. [Installation](#installation)
-2. [Usage](#usage)
-3. [License](#license)
-4. [Contributions](#contributions)
-5. [Tests](#tests)
-6. [Questions](#questions)
-
-## Installation <a id="installation"></a>
-     ${response.installation}
-
-## Usage <a id="usage"></a>
-     ${response.usage}
-
-## License <a id="license"></a>
- When a user chooses a license for their application from a list of options then a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-
-## Contributions <a id="contributions"></a>
-     ${response.contributions}
-
-## Tests <a id="tests"></a>
-     ${response.tests}
-## Questions <a id="questions"></a>
-If you have any further questions, you can find my GitHub profile and email address below. Please reach out and I will do my best to assist.
-GitHub Profile: 
-[www.github.com/${response.github}](https://www.github.com/${response.github})
-Email Address:${response.email}
-
-
-      
-     
-     `, 
-    (err) => err ? console.error(err) : console.log('Success'))
-    
-    
-    
+    setIcon(response)
+    const position = data.indexOf('## Contributions')
+    const badge = `[![License](${licIcon})](${licLink})`
+    const badgePosition = data.slice(0, position) + badge + "\n\n" + data.slice(position)
+    fs.writeFile(`${response.name}.md`, 
+    badgePosition,
+    (err) => err ? console.error(err) : console.log('Success'))   
+ }
 })
+})
+
 
 
 // // function to initialize program
@@ -117,4 +156,4 @@ Email Address:${response.email}
 // }
 
 // // function call to initialize program
-// init();
+// init()
--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,8 +1,53 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
+function generateMarkdown(response) {
+  return`
+# ${response.title}
+
+## Description
+  
+   ${response.description}
+  
+  
+## Table of contents
+  
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [License](#license)
+4. [Contributions](#contributions)
+5. [Tests](#tests)
+6. [Questions](#questions)
+  
+## Installation <a id="installation"></a>
+   ${response.installation}
+  
+## Usage <a id="usage"></a>
+   ${response.usage}
+  
+## License <a id="license"></a>
+When a user chooses a license for their application from a list of options, then a badge for that license is added. ${"\n"}
+   License: ${response.license} ${"\n"} 
+   
+
+## Contributions <a id="contributions"></a>
+   ${response.contributions}
+  
+## Tests <a id="tests"></a>
+   ${response.tests}
+## Questions <a id="questions"></a>
+If you have any further questions, you can find my GitHub profile and email address below. Please reach out and I will do my best to assist.
+${"\n"} GitHub Profile: 
+[www.github.com/${response.github}](https://www.github.com/${response.github}) ${"\n"}
+Email Address: ${response.email}
+  
+  
+        
+       
+       
+
+`
 }
 
-module.exports = generateMarkdown;
+export default generateMarkdown
+
+
